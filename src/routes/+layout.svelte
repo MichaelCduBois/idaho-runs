2e3070420101be85a637634ae3d46789dd0fe3f0
Michael duBois <mdubois@bytement.com>
2023-09-10 16:02:45 -0600

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "../app.postcss";
+
+  // Floating UI for Popups
+  import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
+  import { storePopup } from "@skeletonlabs/skeleton";
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+  // App Shell
+  import { AppShell } from "@skeletonlabs/skeleton";
+</script>
+
+<AppShell>
+  <!-- <svelte:fragment slot="header">Header</svelte:fragment> -->
+  <slot />
+  <!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+  <div class="space-y-5">
+    <h1 class="h1">Idaho Runs Coming Soon!</h1>
+  </div>
+</div>
